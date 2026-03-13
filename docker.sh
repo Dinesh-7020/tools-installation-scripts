23c216fb60fb260f18bc4a6c06d1c31233af5c9b
Dinesh Dusane <67196346+Dinesh-7020@users.noreply.github.com>
2026-03-13 13:32:32 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,7 +4,8 @@ sudo apt update -y
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -   # apt-key is deprecated: newer Debian/Ubuntu versions prefer storing keys as separate files under trusted.gpg.d instead of using apt-key add.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/trusted.gpg.d/docker.gpg > /dev/null  # Use this to save the Docker GPG key as a file under trusted.gpg.d (preferred over apt-key
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
